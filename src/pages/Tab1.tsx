d4f6f49e0a7c956c96abe7fa7beacac2448a5cda
ArjunaWahyu <arjunawahyu0007@gmail.com>
2022-11-21 19:03:35 +0700

    form

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,63 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from "@ionic/react";
+import "./Tab1.css";
+
+
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Form Data</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList class="ion-align-items-center ion-justify-content-center ion-margin">
+          <IonItem
+            counter={true}
+            counterFormatter={(inputLength, maxLength) =>
+              `${maxLength - inputLength} characters remaining`
+            }
+          >
+            <IonLabel position="floating">Nama</IonLabel>
+            <IonInput
+              maxlength={20}
+              type="text"
+              placeholder="Masukkan Nama Anda"
+            ></IonInput>
+          </IonItem>
+          <IonItem
+            counter={true}
+            counterFormatter={(inputLength, maxLength) =>
+              `${maxLength - inputLength} characters remaining`
+            }
+          >
+            <IonLabel position="floating">Tempat</IonLabel>
+            <IonInput
+              maxlength={50}
+              type="text"
+              placeholder="Masukkan Tempat Lahir Anda"
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Tanggal</IonLabel>
+            <IonInput type="date" placeholder="Enter your date"></IonInput>
+          </IonItem>
+          <IonButton id="btnNext" href="/tab2" expand="block" color="primary">
+            Next
+          </IonButton>
+        </IonList>
       </IonContent>
     </IonPage>
   );
